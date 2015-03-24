2e85d03b0c38516e98563d8d6caa35aec230a6ad
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-03-24 15:11:03 +0000

    alteracoes template

--- index.php
+++ index.php
@@ -7,17 +7,10 @@
   <link rel="stylesheet" href="./css/bootstrap.min.css">
   <script src="./js/bootstrap.min.js"></script>
 </head>
-<body>
-
-<div class="container">
-  <div class="jumbotron" style="margin: 0px;">
-      <h1><img src="./imagens/logotipo.png" alt="logotipo" height="150" width="180"> My first Bootstrap website!</h1>     
-  </div>
-
+<body style="height:2000px;">  
     
-    
-    <nav class="navbar navbar-default">
-  <div class="container-fluid">
+    <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -26,7 +19,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="#">Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -34,25 +27,8 @@
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
         <li class="dropdown">
@@ -70,12 +46,17 @@
   </div><!-- /.container-fluid -->
 </nav>
     
+    <!-- /conteudo -->
+    <div class="container theme-showcase" role="main">
+    
+    <div class="jumbotron">
+      <img src="./imagens/logotipo-banner.png" alt="logotipo" height="150" width="1140">   
+  </div><!-- /banner -->
+    
     
     
   
 
-  
-</div>
-
+    </div>
 </body>
 </html>
