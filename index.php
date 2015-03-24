b355ca405adce29b559073a50bd62037e6bf7bb5
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-03-24 15:20:45 +0000

    ad

--- index.php
+++ index.php
@@ -12,13 +12,14 @@
     <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
+    <div class="navbar-header">  
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-      </button>
+      </button> 
+        <a class="navbar-brand" href="#"><img src="./imagens/logotipo-pequeno-menu.png" alt="logotipo" style="margin-top:0px;"></a>
       <a class="navbar-brand" href="#">Home</a>
     </div>
 
