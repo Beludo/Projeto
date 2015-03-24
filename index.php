cd23444424b7410f099b55de2dfc6d9e96ec2450
Diogo Novais <dnovais17@gmail.com>
2015-03-24 15:42:14 +0000

    Correções na imagem do menu e itens de menu

--- index.php
+++ index.php
@@ -19,19 +19,23 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button> 
-        <a class="navbar-brand" href="#"><img src="./imagens/logotipo-pequeno-menu.png" alt="logotipo" style="margin-top:0px;"></a>
-      <a class="navbar-brand" href="#">Home</a>
+        <a class="navbar-brand" style="padding: 5px 5px" href="#"><img src="./imagens/logotipo-pequeno-menu.png" alt="logotipo" style="margin-top:0px;"></a>
+      <a class="navbar-brand" href="#">Inicio</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li class="active"><a href="#">Visita virtual</a></li>
+        <li><a href="#">Loja</a></li>
+		<li><a href="#">Exposições</a></li>
+		<li><a href="#">Ligações</a></li>
+		<li><a href="#">Atividades</a></li>
+		<li><a href="#">Contatos</a></li>
       </ul>
 
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
+        <li><a href="#">Iniciar sessão</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
