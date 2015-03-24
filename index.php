3c5e6646d50c61e060dc76aefdabd554378e8309
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-03-24 15:51:19 +0000

    ja está

--- index.php
+++ index.php
@@ -1,76 +1,92 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
-  <title>Museu</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./css/bootstrap.min.css">
-  <script src="./js/bootstrap.min.js"></script>
+    <title>Museu</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/bootstrap.min.js"></script>
 </head>
-<body style="height:2000px;">  
-    
+
+<body style="height:2000px;">
+
     <nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">  
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button> 
-        <a class="navbar-brand" style="padding: 5px 5px" href="#"><img src="./imagens/logotipo-pequeno-menu.png" alt="logotipo" style="margin-top:0px;"></a>
-      <a class="navbar-brand" href="#">Inicio</a>
-    </div>
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" style="padding: 5px 5px" href="#"><img src="./imagens/logotipo-pequeno-menu.png" alt="logotipo" style="margin-top:0px;">
+                </a>
+                <a class="navbar-brand" href="#">Inicio</a>
+            </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Visita virtual</a></li>
-        <li><a href="#">Loja</a></li>
-		<li><a href="#">Exposições</a></li>
-		<li><a href="#">Ligações</a></li>
-		<li><a href="#">Atividades</a></li>
-		<li><a href="#">Contatos</a></li>
-      </ul>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Visita virtual</a>
+                    </li>
+                    <li><a href="#">Loja</a>
+                    </li>
+                    <li><a href="#">Exposições</a>
+                    </li>
+                    <li><a href="#">Ligações</a>
+                    </li>
+                    <li><a href="#">Atividades</a>
+                    </li>
+                    <li><a href="#">Contatos</a>
+                    </li>
+                </ul>
 
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Iniciar sessão</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-    
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Iniciar sessão</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
     <!-- /conteudo -->
     <div class="container theme-showcase" role="main">
-    
+
         <div class="jumbotron" style="margin:1px;">
-    <img src="./imagens/logotipo-banner.png" alt="logotipo" height="150" width="1140">
-</div>
-<!-- /banner -->
-
-<div id="noticias e outras cenas" style="float:left; margin:1px;">
-
-    <div class="panel panel-default">
-        <div class="panel-heading">Panel heading without title</div>
-        <div class="panel-body">
-            Panel content
+            <img src="./imagens/logotipo-banner.png" alt="logotipo" height="150" width="1140">
         </div>
-    </div>
+        <!-- /banner -->
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Panel title</h3>
+        <div id="noticias e outras cenas" style="float:left; margin:1px;">
+
+
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Noticias</h3>
+                </div>
+                <div class="panel-body">
+                    Panel content
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Outras Cenas</h3>
+                </div>
+                <div class="panel-body">
+                    Panel content
+                </div>
+            </div>
         </div>
-        <div class="panel-body">
-            Panel content
-        </div>
-    </div>
-</div>
-    
-    
-  
+
+
+
 
     </div>
 </body>
+
 </html>
