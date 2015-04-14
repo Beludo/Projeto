1af190d704af8a6d51a889c36766bbfb5bc09884
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-14 11:54:09 +0100

    :sa:

---
+++ ligacoes.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+	<title>Museu</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./css/bootstrap.min.css">
+	<script src="./js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<!-- MENU!! -->
+	<?php 
+        $pagina = "informacoes";
+        include "inc-cabecalho.php" ?>
+	<!-- Acaba MENU!! -->
+
+	<!-- /conteudo -->
+	<div class="container theme-showcase" role="main">
+
+		<div class="jumbotron" style="margin:1px; padding-bottom:1px;">
+			<img src="./imagens/logotipo-banner.png" alt="logotipo">
+		</div>
+		<!-- /banner -->
+		
+		 <!-- BREADCRUMB -->
+		<ol class="breadcrumb" style="margin-bottom:1px;">
+			<li><a href="index.php">Página Inicial</a>
+			</li><li><a href="contatos.php">Informações</a>
+			</li>
+			<li class="active">Ligações</li>
+		</ol>
+		
+		<!-- Menu Lateral -->
+		<div style="float:left; margin-top:10px; margin-right:0px; width:25%;">
+			<div class="list-group">
+			<!-- Exemplo de opção ativa
+			
+			  <a href="#" class="list-group-item active">
+				Cras justo odio
+			  </a>
+			  -->
+			  <a href="contatos.php" class="list-group-item">Contatos</a>
+			  <a href="horario.php" class="list-group-item">Horário de Funcionamento</a>
+			  <a href="mapasite.php" class="list-group-item">Mapa do Site</a>
+			  <a href="normas.php" class="list-group-item">Normas de Conduta</a>
+              <a href="ligacoes.php" class="list-group-item active">Ligações</a>
+                
+			</div>
+		</div>
+		<!-- Acaba menu Lateral -->
+
+		<!-- Conteudo -->
+		<div class="panel panel-default" style="float:right; padding:10px; margin-top:10px; width:74%;">
+			   Conteudo principal <br>
+					Linha 1 <br>
+					Linha 2 <br>
+					Linha 3 <br>
+		</div>
+		<!-- Acaba o conteudo -->
+		
+	</div>
+	
+	<!-- RODAPÉ!! -->
+	<?php include "inc-rodape.php" ?>
+	<!-- Acaba RODAPÉ!! -->
+	
+	<script src="./js/jquery.min.js"></script>
+	<script src="./js/bootstrap.min.js"></script>
+</body>
+</html>
