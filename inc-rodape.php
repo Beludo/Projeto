444a0596ddfc38b4c7b7f77083de09b94b8fe49a
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-14 10:22:36 +0100

    tens ai novais

--- inc-rodape.php
+++ inc-rodape.php
@@ -1,11 +1,11 @@
 <footer>
-	<div class="centered">
+	<div class="text-center">
 		<hr>
 		© Copyright Gestor de Museus 2015. Todos os direitos reservados.<br><br>
 		<a href="mapasite.php">Mapa do site</a> |
 		<a href="contatos.php">Contatos</a> |
 		<a href="horario.php">Horário de funcionamento</a> |
-		<a href="normas.php">Normas de funcionamento</a> |
+		<a href="normas.php">Normas de funcionamento</a>
 		<br>
 	</div>
 </footer>
