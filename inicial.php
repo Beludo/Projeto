450671897bb32c706bd382d0e4e1ab15ab02d947
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-14 17:14:10 +0100

    atualizacao do initial na parte do menu

--- inicial.php
+++ inicial.php
@@ -12,39 +12,37 @@
 <body>
     <!-- MENU!! -->
     <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" style="padding: 5px 5px" href="#"><img src="./imagens/logotipo-pequeno-menu.png" alt="logotipo" style="margin-top:0px;">
-                </a>
-            </div>
+	<div class="container">
+		<!-- O logotipo e o menu para telemovel estão agrupados -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Menu pequeno</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" style="padding: 5px 5px" href="index.php"><img src="./imagens/logotipo-pequeno-menu.png" alt="logotipo" style="margin-top:0px;">
+			</a>
+		</div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="./index.php">Página Inicial</a>
-                    </li>
-                    <li><a href="#">Visita virtual</a>
-                    </li>
-                    <li><a href="#">Loja</a>
-                    </li>
-                    <li><a href="./exposicoes.php">Exposições</a>
-                    </li>
-                    <li><a href="#">Ligações</a>
-                    </li>
-                    <li><a href="#">Atividades</a>
-                    </li>
-                    <li><a href="contatos.php">Informações</a>
-                    </li>
-                </ul>
+		<!-- Guardar as opções de menu para o menu movel (lá pra cima) -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php">Página Inicial</a>
+				</li>
+				<li ><a href="visitavirtual.php">Visita virtual</a>
+				</li>
+				<li><a href="loja.php">Loja</a>
+				</li>
+				<li><a href="exposicoes.php">Exposições</a>
+				</li>
+				<li><a href="eventos.php">Eventos</a>
+				</li>
+				<li><a href="contatos.php">Informações</a>
+				</li>
+			</ul>
 
-                <ul class="nav navbar-nav navbar-right">
+			<ul class="nav navbar-nav navbar-right">
                     <li class="dropdown" role="menu" aria-labelledby="menu1" style="z-index: 1">
                         <a id="menu1" data-toggle="dropdown">Zé Tobias<span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
@@ -58,11 +56,20 @@
                     </li>
 
                 </ul>
+			 <ul class="nav navbar-nav navbar-right">
+                 <li>
+		   <form class="navbar-form navbar-left" role="search" method="POST">
+			   <input type="text" id="pesquisa" name="pesquisa" class="form-control" placeholder="Search">
+			</form></li>
+                 
+			</ul>
+
+                
             </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
+		<!-- /.navbar-collapse -->
+	</div>
+	<!-- /.container-fluid -->
+</nav>
     <!-- Acaba MENU!! -->
 
     <!-- /conteudo -->
