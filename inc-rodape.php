4bfa01808574823b98a734349d8f26af7ae68567
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-14 11:12:33 +0100

    Novais :feelsgood: :muscle:

--- inc-rodape.php
+++ inc-rodape.php
@@ -5,7 +5,8 @@
 		<a href="mapasite.php">Mapa do site</a> |
 		<a href="contatos.php">Contatos</a> |
 		<a href="horario.php">Horário de funcionamento</a> |
-		<a href="normas.php">Normas de funcionamento</a>
+		<a href="normas.php">Normas de funcionamento</a> |
+        <a href="ligacoes.php">Ligações</a>
 		<br><br><br>
 	</div>
 </footer>
