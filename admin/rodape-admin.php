f4d9955c8e9bf45e7a731fb8d4507cd0164a9d77
Diogo Novais <dnovais17@gmail.com>
2015-04-16 14:41:02 +0100

    Atualização do rodapé

---
+++ admin/rodape-admin.php
@@ -0,0 +1,6 @@
+  <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Versão</b> 0.1
+        </div>
+        <strong>Copyright &copy; 2015 <a href="http://www.facebook.com/dnovais91">Novais</a>, <a href="http://www.facebook.com/diogoalexandre99">Ressureição</a> e <a href="http://www.facebook.com/marco.beludo">Beludo</a>.</strong> Todos os direitos reservados.
+  </footer>
