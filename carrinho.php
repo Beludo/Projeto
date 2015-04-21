24b9541623be30395787791875913d4de8bbb293
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-21 14:42:18 +0100

    Alteração prototipo carrinho :smirk_cat:

--- carrinho.php
+++ carrinho.php
@@ -90,106 +90,97 @@
 		</ol>
 
 		<!-- Conteudo -->
-		<div class="panel-default" style="padding:10px; margin-top:10px;">
-
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-12" st>
-						<div class="panel panel-info">
-							<div class="panel-heading">
-								<div class="panel-title">
-									<div class="row">
-										<div class="col-xs-6">
-											<h5><span class="glyphicon glyphicon-shopping-cart"></span> Carrinho de Compras</h5>
-										</div>
-										<div class="col-xs-6">
-											<button type="button" class="btn btn-primary btn-sm btn-block">
-												<span class="glyphicon glyphicon-share-alt"></span> Continuar a Comprar
-											</button>
-										</div>
-									</div>
-								</div>
+		<div class="panel-default" style="margin-top:10px;">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<div class="panel-title">
+						<div class="row">
+							<div class="col-xs-6">
+								<h5><span class="glyphicon glyphicon-shopping-cart"></span> Carrinho de Compras</h5>
 							</div>
-							<div class="panel-body">
-								<div class="row">
-									<div class="col-xs-2"><img class="img-responsive" src="http://placehold.it/100x70">
-									</div>
-									<div class="col-xs-4">
-										<h4 class="product-name"><strong>Nome do Produto</strong></h4>
-										<h4><small>Descrição do Produto</small></h4>
-									</div>
-									<div class="col-xs-6">
-										<div class="col-xs-6 text-right">
-											<h6><strong>25.00 <span class="text-muted">x</span></strong></h6>
-										</div>
-										<div class="col-xs-4">
-											<input type="text" class="form-control input-sm" value="1">
-										</div>
-										<div class="col-xs-2">
-											<button type="button" class="btn btn-link btn-xs">
-												<span class="glyphicon glyphicon-trash"> </span>
-											</button>
-										</div>
-									</div>
-								</div>
-								<hr>
-								<div class="row">
-									<div class="col-xs-2"><img class="img-responsive" src="http://placehold.it/100x70">
-									</div>
-									<div class="col-xs-4">
-										<h4 class="product-name"><strong>Nome do Produto</strong></h4>
-										<h4><small>Descrição do Produto</small></h4>
-									</div>
-									<div class="col-xs-6">
-										<div class="col-xs-6 text-right">
-											<h6><strong>25.00 <span class="text-muted">x</span></strong></h6>
-										</div>
-										<div class="col-xs-4">
-											<input type="text" class="form-control input-sm" value="1">
-										</div>
-										<div class="col-xs-2">
-											<button type="button" class="btn btn-link btn-xs">
-												<span class="glyphicon glyphicon-trash"> </span>
-											</button>
-										</div>
-									</div>
-								</div>
-								<hr>
-								<div class="row">
-									<div class="text-center">
-										<div class="col-xs-9">
-											<h6 class="text-right">Adicionou items?</h6>
-										</div>
-										<div class="col-xs-3">
-											<button type="button" class="btn btn-default btn-sm btn-block">
-												Atualizar Carrinho
-											</button>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="panel-footer">
-								<div class="row text-center">
-									<div class="col-xs-9">
-										<h4 class="text-right">Total <strong>€50.00</strong></h4>
-									</div>
-									<div class="col-xs-3">
-										<button type="button" class="btn btn-success btn-block">
-											Checkout
-										</button>
-									</div>
-								</div>
+							<div class="col-xs-6">
+								<button type="button" class="btn btn-primary btn-sm btn-block">
+									<span class="glyphicon glyphicon-share-alt"></span> Continuar a Comprar
+								</button>
 							</div>
 						</div>
 					</div>
 				</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-xs-2"><img class="img-responsive" src="http://placehold.it/100x70">
+						</div>
+						<div class="col-xs-4">
+							<h4 class="product-name"><strong>Nome do Produto</strong></h4>
+							<h4><small>Descrição do Produto</small></h4>
+						</div>
+						<div class="col-xs-6">
+							<div class="col-xs-6 text-right">
+								<h6><strong>25.00 <span class="text-muted">x</span></strong></h6>
+							</div>
+							<div class="col-xs-4">
+								<input type="text" class="form-control input-sm" value="1">
+							</div>
+							<div class="col-xs-2">
+								<button type="button" class="btn btn-link btn-xs">
+									<span class="glyphicon glyphicon-trash"> </span>
+								</button>
+							</div>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-xs-2"><img class="img-responsive" src="http://placehold.it/100x70">
+						</div>
+						<div class="col-xs-4">
+							<h4 class="product-name"><strong>Nome do Produto</strong></h4>
+							<h4><small>Descrição do Produto</small></h4>
+						</div>
+						<div class="col-xs-6">
+							<div class="col-xs-6 text-right">
+								<h6><strong>25.00 <span class="text-muted">x</span></strong></h6>
+							</div>
+							<div class="col-xs-4">
+								<input type="text" class="form-control input-sm" value="1">
+							</div>
+							<div class="col-xs-2">
+								<button type="button" class="btn btn-link btn-xs">
+									<span class="glyphicon glyphicon-trash"> </span>
+								</button>
+							</div>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="text-center">
+							<div class="col-xs-9">
+								<h6 class="text-right">Adicionou items?</h6>
+							</div>
+							<div class="col-xs-3">
+								<button type="button" class="btn btn-default btn-sm btn-block">
+									Atualizar Carrinho
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-footer">
+					<div class="row text-center">
+						<div class="col-xs-9">
+							<h4 class="text-right">Total <strong>€50.00</strong></h4>
+						</div>
+						<div class="col-xs-3">
+							<button type="button" class="btn btn-success btn-block">
+								Continuar
+							</button>
+						</div>
+					</div>
+				</div>
 			</div>
-
-
 		</div>
-		<!-- Acaba o conteudo -->
-
 	</div>
+
+	<!-- Acaba o conteudo -->
 
 	<!-- RODAPÉ!! -->
 	<?php include "inc-rodape.php" ?>
