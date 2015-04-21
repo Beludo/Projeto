24187d6c9715d23896ce426318a0930f1c4f6864
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-21 11:51:19 +0100

    adicionei uma hiperligação às imagens em carrossel do prototipo loja!

--- loja.php
+++ loja.php
@@ -62,13 +62,13 @@
                             </ol>
                             <div class="carousel-inner">
                                 <div class="item active">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <a href="produto.php"><img class="slide-image" src="http://placehold.it/800x300" alt=""></a>
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <a href="produto.php"><img class="slide-image" src="http://placehold.it/800x300" alt=""></a>
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <a href="produto.php"><img class="slide-image" src="http://placehold.it/800x300" alt=""></a>
                                 </div>
                             </div>
                             <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
