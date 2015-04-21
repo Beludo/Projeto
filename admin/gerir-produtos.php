3451ff7dcaa819518ea7fa443d63fff567b0c839
Diogo Novais <dnovais17@gmail.com>
2015-04-21 16:04:06 +0100

    Gerir produtos

---
+++ admin/gerir-produtos.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="UTF-8">
+	<title>Admin | Gerir produtos</title>
+	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+	<!-- Bootstrap 3.3.2 -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<!-- Font Awesome Icons -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<!-- Ionicons -->
+	<link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+	<!-- DATA TABLES -->
+	<link href="plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+	<!-- Theme style -->
+	<link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+	<!-- AdminLTE Skins. Choose a skin from the css/skins 
+		 folder instead of downloading all of them to reduce the load. -->
+	<link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+  </head>
+  <body class="skin-blue">
+	<div class="wrapper">
+	  
+	  <?php
+		include("cabecalho-admin.php");
+		include("lateral-admin.php");
+	  ?>
+
+	  <!-- Content Wrapper. Contains page content -->
+	  <div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+		  <h1>
+			Gerir produtos
+			<small>descrição</small>
+		  </h1>
+		  <ol class="breadcrumb">
+			<li><a href="index.php"><i class="fa fa-dashboard"></i> Inicio</a></li>
+			<li class="active"><a href="#">Gestão de produtos</a></li>
+		  </ol>
+		</section>
+
+		<!-- Main content -->
+		<section class="content">
+		  <div class="row">
+			<div class="col-xs-12">
+			  <div class="box">
+				<div class="box-header">
+				  <h3 class="box-title">Produtos registados</h3>
+				</div><!-- /.box-header -->
+				<div class="box-body">
+				  <table id="example1" class="table table-bordered table-striped">
+					<thead>
+					  <tr>
+						<th>Imagem</th>
+						<th>Nome</th>
+						<th>Código</th>
+						<th>Preço</th>
+						<th>Stock</th>
+						<th>Descrição</th>
+					  </tr>
+					</thead>
+					<tbody>
+					  <tr>
+						<td><img src=""></td>
+						<td>T-shirt do museu</td>
+						<td>TM123</td>
+						<td>5€</td>
+						<td>10</td>
+						<td>
+						  <div class="btn-group">
+							<button aria-expanded="false" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+							  <span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu">
+							  <li><a href="#"><i class="fa fa-fw fa-edit"></i>Editar</a></li>
+							  <li><a href="#"><i class="fa fa-fw fa-plus-square"></i>Ativar</a></li>
+							  <li><a href="#"><i class="fa fa-fw fa-minus-square"></i>Desativar</a></li>
+							</ul>
+						  </div>
+						</td>
+					  </tr>
+					  <tr>
+						<td>Diogo Ressureição</td>
+						<td>diogo.alenxandre99</td>
+						<td>diogo.alenxandre99@hotmail.com</td>
+						<td>Desativo</td>
+						<td>
+						  <div class="btn-group">
+							<button aria-expanded="false" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+							  <span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu">
+							  <li><a href="editar-utilizador.php"><i class="fa fa-fw fa-edit"></i>Editar</a></li>
+							  <li><a href="#"><i class="fa fa-fw fa-plus-square"></i>Ativar</a></li>
+							  <li><a href="#"><i class="fa fa-fw fa-minus-square"></i>Desativar</a></li>
+							</ul>
+						  </div>
+						</td>
+					  </tr>
+					</tfoot>
+				  </table>
+				</div><!-- /.box-body -->
+			  </div><!-- /.box -->
+			</div><!-- /.col -->
+		  </div><!-- /.row -->
+		</section><!-- /.content -->
+	  </div><!-- /.content-wrapper -->
+	  
+	  <?php
+		include("rodape-admin.php");
+	  ?>
+	</div><!-- ./wrapper -->
+
+	<!-- jQuery 2.1.3 -->
+	<script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+	<!-- Bootstrap 3.3.2 JS -->
+	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<!-- DATA TABES SCRIPT -->
+	<script src="plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+	<script src="plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+	<!-- SlimScroll -->
+	<script src="plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+	<!-- FastClick -->
+	<script src='plugins/fastclick/fastclick.min.js'></script>
+	<!-- AdminLTE App -->
+	<script src="dist/js/app.min.js" type="text/javascript"></script>
+	<!-- AdminLTE for demo purposes -->
+	<script src="dist/js/demo.js" type="text/javascript"></script>
+	<!-- page script -->
+	<script type="text/javascript">
+	  $(function () {
+		$("#example1").dataTable();
+		$('#example2').dataTable({
+		  "bPaginate": true,
+		  "bLengthChange": false,
+		  "bFilter": false,
+		  "bSort": true,
+		  "bInfo": true,
+		  "bAutoWidth": false
+		});
+	  });
+	</script>
+
+  </body>
+</html>
