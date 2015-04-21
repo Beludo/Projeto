08171a5521477946f04ab2cd94d806be3c211373
Diogo Novais <dnovais17@gmail.com>
2015-04-21 11:11:43 +0100

    Gráficos de temperatura

---
+++ admin/graficos-temp.php
@@ -0,0 +1,473 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="UTF-8">
+	<title>Admin | Graficos de temperatura</title>
+	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+	<!-- Bootstrap 3.3.2 -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<!-- Font Awesome Icons -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<!-- Ionicons -->
+	<link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+	<!-- Theme style -->
+	<link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+	<!-- AdminLTE Skins. Choose a skin from the css/skins 
+		 folder instead of downloading all of them to reduce the load. -->
+	<link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+	<!-- Theme style -->
+    <link href="plugins/iCheck/all.css" rel="stylesheet" type="text/css" />
+    <!-- daterange picker -->
+    <link href="plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+    <!-- Bootstrap time Picker -->
+    <link href="plugins/timepicker/bootstrap-timepicker.min.css" rel="stylesheet"/>
+	
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+  </head>
+  <body class="skin-blue">
+	<div class="wrapper">
+	  
+	<?php
+		include("cabecalho-admin.php");
+		include("lateral-admin.php");
+	?>
+
+	  <!-- Content Wrapper. Contains page content -->
+	  <div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+		  <h1>
+			Graficos de temperatura
+			<small>Texto pequeno</small>
+		  </h1>
+		  <ol class="breadcrumb">
+			<li><a href="index.php"><i class="fa fa-dashboard"></i> Inicio</a></li>
+			<li class="active">Graficos de temperatura</li>
+		  </ol>
+		</section>
+
+		<!-- Main content -->
+		<section class="content">
+			<div class="row">
+				<div class="col-xs-12">
+				  <!-- general form elements -->
+				  <div class="box box-primary">
+					<div class="box-header">
+					  <h3 class="box-title">Registo da temperatura</h3>
+					</div><!-- /.box-header -->
+					<!-- form start -->
+					<form role="form">
+						<div class="box-body">
+							<div class="form-group">
+								<label>Intervalo de tempo a mostrar:</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-clock-o"></i>
+									</div>
+									<input class="form-control pull-right" id="reservationtime" type="text">
+								</div><!-- /.input group -->
+							</div>
+							<div class="box-footer">
+								<button type="submit" class="btn btn-primary">Atualizar gráficos</button>
+							</div>
+						</div>
+					</form>
+				  </div><!-- /.box -->
+				</div><!-- /.col -->
+			</div><!-- /.row -->
+		
+			<div class="row">
+			  <div class="col-md-6">
+				  <!-- Line chart -->
+				  <div class="box box-primary">
+					<div class="box-header">
+					  <i class="fa fa-bar-chart-o"></i>
+					  <h3 class="box-title">Line Chart</h3>
+					</div>
+					<div class="box-body">
+					  <div id="line-chart" style="height: 300px; padding: 0px; position: relative;"><canvas height="300" width="467" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 467px; height: 300px;" class="flot-base"></canvas><div style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; font-size: smaller; color: rgb(84, 84, 84);" class="flot-text"><div style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; display: block;" class="flot-x-axis flot-x1-axis xAxis x1Axis"><div class="flot-tick-label tickLabel" style="position: absolute; max-width: 58px; top: 283px; left: 20px; text-align: center;">0</div><div class="flot-tick-label tickLabel" style="position: absolute; max-width: 58px; top: 283px; left: 84px; text-align: center;">2</div><div class="flot-tick-label tickLabel" style="position: absolute; max-width: 58px; top: 283px; left: 149px; text-align: center;">4</div><div class="flot-tick-label tickLabel" style="position: absolute; max-width: 58px; top: 283px; left: 214px; text-align: center;">6</div><div class="flot-tick-label tickLabel" style="position: absolute; max-width: 58px; top: 283px; left: 278px; text-align: center;">8</div><div class="flot-tick-label tickLabel" style="position: absolute; max-width: 58px; top: 283px; left: 341px; text-align: center;">10</div><div class="flot-tick-label tickLabel" style="position: absolute; max-width: 58px; top: 283px; left: 405px; text-align: center;">12</div></div><div style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; display: block;" class="flot-y-axis flot-y1-axis yAxis y1Axis"><div class="flot-tick-label tickLabel" style="position: absolute; top: 270px; left: 1px; text-align: right;">-1.5</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 225px; left: 1px; text-align: right;">-1.0</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 180px; left: 1px; text-align: right;">-0.5</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 135px; left: 4px; text-align: right;">0.0</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 90px; left: 4px; text-align: right;">0.5</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 45px; left: 4px; text-align: right;">1.0</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 0px; left: 4px; text-align: right;">1.5</div></div></div><canvas height="300" width="467" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 467px; height: 300px;" class="flot-overlay"></canvas></div>
+					</div><!-- /.box-body-->
+				  </div><!-- /.box -->
+				</div>
+			</div>   <!-- /.row -->
+		  
+		</section><!-- /.content -->
+	  </div><!-- /.content-wrapper -->
+	  
+	  <?php
+		include("rodape-admin.php");
+	  ?>
+	</div><!-- ./wrapper -->
+
+	<!-- jQuery 2.1.3 -->
+	<script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+	<!-- Bootstrap 3.3.2 JS -->
+	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<!-- InputMask -->
+    <script src="plugins/input-mask/jquery.inputmask.js" type="text/javascript"></script>
+    <script src="plugins/input-mask/jquery.inputmask.date.extensions.js" type="text/javascript"></script>
+    <script src="plugins/input-mask/jquery.inputmask.extensions.js" type="text/javascript"></script>
+	<!-- bootstrap time picker -->
+    <script src="plugins/timepicker/bootstrap-timepicker.min.js" type="text/javascript"></script>
+	<!-- SlimScroll 1.3.0 -->
+    <script src="plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+	<!-- iCheck 1.0.1 -->
+    <script src="plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+	
+	<!-- FastClick -->
+	<script src='plugins/fastclick/fastclick.min.js'></script>
+	<!-- AdminLTE App -->
+	<script src="dist/js/app.min.js" type="text/javascript"></script>
+	<!-- AdminLTE for demo purposes -->
+	<script src="dist/js/demo.js" type="text/javascript"></script>
+	<!-- date-range-picker -->
+    <script src="plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+	<!-- bootstrap time picker -->
+    <script src="plugins/timepicker/bootstrap-timepicker.min.js" type="text/javascript"></script>
+	
+	<!-- FLOT CHARTS -->
+    <script src="/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+    <!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
+    <script src="/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+    <!-- FLOT PIE PLUGIN - also used to draw donut charts -->
+    <script src="/plugins/flot/jquery.flot.pie.min.js" type="text/javascript"></script>
+    <!-- FLOT CATEGORIES PLUGIN - Used to draw bar charts -->
+    <script src="/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
+	
+	<!-- Script do seletor de data/hora -->
+    <script type="text/javascript">
+      $(function () {
+        //Datemask dd/mm/yyyy
+        $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
+        //Datemask2 mm/dd/yyyy
+        $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
+        //Money Euro
+        $("[data-mask]").inputmask();
+
+        //Date range picker
+        $('#reservation').daterangepicker();
+        //Date range picker with time picker
+        $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
+        //Date range as a button
+        $('#daterange-btn').daterangepicker(
+                {
+                  ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                    'Last 7 Days': [moment().subtract('days', 6), moment()],
+                    'Last 30 Days': [moment().subtract('days', 29), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+                  },
+                  startDate: moment().subtract('days', 29),
+                  endDate: moment()
+                },
+        function (start, end) {
+          $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+        );
+
+        //iCheck for checkbox and radio inputs
+        $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+          checkboxClass: 'icheckbox_minimal-blue',
+          radioClass: 'iradio_minimal-blue'
+        });
+        //Red color scheme for iCheck
+        $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+          checkboxClass: 'icheckbox_minimal-red',
+          radioClass: 'iradio_minimal-red'
+        });
+        //Flat red color scheme for iCheck
+        $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+          checkboxClass: 'icheckbox_flat-green',
+          radioClass: 'iradio_flat-green'
+        });
+
+        //Colorpicker
+        $(".my-colorpicker1").colorpicker();
+        //color picker with addon
+        $(".my-colorpicker2").colorpicker();
+
+        //Timepicker
+        $(".timepicker").timepicker({
+          showInputs: false
+        });
+		
+		<!-- Script do seletor de data -->
+		$("#example1").dataTable();
+		$('#example2').dataTable({
+		  "bPaginate": true,
+		  "bLengthChange": false,
+		  "bFilter": false,
+		  "bSort": true,
+		  "bInfo": true,
+		  "bAutoWidth": false
+		});
+		
+		<!-- Script para mostrar os gráficos -->
+		/*
+         * Flot Interactive Chart
+         * -----------------------
+         */
+        // We use an inline data source in the example, usually data would
+        // be fetched from a server
+        var data = [], totalPoints = 100;
+        function getRandomData() {
+
+          if (data.length > 0)
+            data = data.slice(1);
+
+          // Do a random walk
+          while (data.length < totalPoints) {
+
+            var prev = data.length > 0 ? data[data.length - 1] : 50,
+                    y = prev + Math.random() * 10 - 5;
+
+            if (y < 0) {
+              y = 0;
+            } else if (y > 100) {
+              y = 100;
+            }
+
+            data.push(y);
+          }
+
+          // Zip the generated y values with the x values
+          var res = [];
+          for (var i = 0; i < data.length; ++i) {
+            res.push([i, data[i]]);
+          }
+
+          return res;
+        }
+
+        var interactive_plot = $.plot("#interactive", [getRandomData()], {
+          grid: {
+            borderColor: "#f3f3f3",
+            borderWidth: 1,
+            tickColor: "#f3f3f3"
+          },
+          series: {
+            shadowSize: 0, // Drawing is faster without shadows
+            color: "#3c8dbc"
+          },
+          lines: {
+            fill: true, //Converts the line chart to area chart
+            color: "#3c8dbc"
+          },
+          yaxis: {
+            min: 0,
+            max: 100,
+            show: true
+          },
+          xaxis: {
+            show: true
+          }
+        });
+
+        var updateInterval = 500; //Fetch data ever x milliseconds
+        var realtime = "on"; //If == to on then fetch data every x seconds. else stop fetching
+        function update() {
+
+          interactive_plot.setData([getRandomData()]);
+
+          // Since the axes don't change, we don't need to call plot.setupGrid()
+          interactive_plot.draw();
+          if (realtime === "on")
+            setTimeout(update, updateInterval);
+        }
+
+        //INITIALIZE REALTIME DATA FETCHING
+        if (realtime === "on") {
+          update();
+        }
+        //REALTIME TOGGLE
+        $("#realtime .btn").click(function () {
+          if ($(this).data("toggle") === "on") {
+            realtime = "on";
+          }
+          else {
+            realtime = "off";
+          }
+          update();
+        });
+        /*
+         * END INTERACTIVE CHART
+         */
+
+
+        /*
+         * LINE CHART
+         * ----------
+         */
+        //LINE randomly generated data
+
+        var sin = [], cos = [];
+        for (var i = 0; i < 14; i += 0.5) {
+          sin.push([i, Math.sin(i)]);
+          cos.push([i, Math.cos(i)]);
+        }
+        var line_data1 = {
+          data: sin,
+          color: "#3c8dbc"
+        };
+        var line_data2 = {
+          data: cos,
+          color: "#00c0ef"
+        };
+        $.plot("#line-chart", [line_data1, line_data2], {
+          grid: {
+            hoverable: true,
+            borderColor: "#f3f3f3",
+            borderWidth: 1,
+            tickColor: "#f3f3f3"
+          },
+          series: {
+            shadowSize: 0,
+            lines: {
+              show: true
+            },
+            points: {
+              show: true
+            }
+          },
+          lines: {
+            fill: false,
+            color: ["#3c8dbc", "#f56954"]
+          },
+          yaxis: {
+            show: true,
+          },
+          xaxis: {
+            show: true
+          }
+        });
+        //Initialize tooltip on hover
+        $("<div class='tooltip-inner' id='line-chart-tooltip'></div>").css({
+          position: "absolute",
+          display: "none",
+          opacity: 0.8
+        }).appendTo("body");
+        $("#line-chart").bind("plothover", function (event, pos, item) {
+
+          if (item) {
+            var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+
+            $("#line-chart-tooltip").html(item.series.label + " of " + x + " = " + y)
+                    .css({top: item.pageY + 5, left: item.pageX + 5})
+                    .fadeIn(200);
+          } else {
+            $("#line-chart-tooltip").hide();
+          }
+
+        });
+        /* END LINE CHART */
+
+        /*
+         * FULL WIDTH STATIC AREA CHART
+         * -----------------
+         */
+        var areaData = [[2, 88.0], [3, 93.3], [4, 102.0], [5, 108.5], [6, 115.7], [7, 115.6],
+          [8, 124.6], [9, 130.3], [10, 134.3], [11, 141.4], [12, 146.5], [13, 151.7], [14, 159.9],
+          [15, 165.4], [16, 167.8], [17, 168.7], [18, 169.5], [19, 168.0]];
+        $.plot("#area-chart", [areaData], {
+          grid: {
+            borderWidth: 0
+          },
+          series: {
+            shadowSize: 0, // Drawing is faster without shadows
+            color: "#00c0ef"
+          },
+          lines: {
+            fill: true //Converts the line chart to area chart
+          },
+          yaxis: {
+            show: false
+          },
+          xaxis: {
+            show: false
+          }
+        });
+
+        /* END AREA CHART */
+
+        /*
+         * BAR CHART
+         * ---------
+         */
+
+        var bar_data = {
+          data: [["January", 10], ["February", 8], ["March", 4], ["April", 13], ["May", 17], ["June", 9]],
+          color: "#3c8dbc"
+        };
+        $.plot("#bar-chart", [bar_data], {
+          grid: {
+            borderWidth: 1,
+            borderColor: "#f3f3f3",
+            tickColor: "#f3f3f3"
+          },
+          series: {
+            bars: {
+              show: true,
+              barWidth: 0.5,
+              align: "center"
+            }
+          },
+          xaxis: {
+            mode: "categories",
+            tickLength: 0
+          }
+        });
+        /* END BAR CHART */
+
+        /*
+         * DONUT CHART
+         * -----------
+         */
+
+        var donutData = [
+          {label: "Series2", data: 30, color: "#3c8dbc"},
+          {label: "Series3", data: 20, color: "#0073b7"},
+          {label: "Series4", data: 50, color: "#00c0ef"}
+        ];
+        $.plot("#donut-chart", donutData, {
+          series: {
+            pie: {
+              show: true,
+              radius: 1,
+              innerRadius: 0.5,
+              label: {
+                show: true,
+                radius: 2 / 3,
+                formatter: labelFormatter,
+                threshold: 0.1
+              }
+
+            }
+          },
+          legend: {
+            show: false
+          }
+        });
+        /*
+         * END DONUT CHART
+         */
+		
+      });
+	  
+	  function labelFormatter(label, series) {
+        return "<div style='font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;'>"
+                + label
+                + "<br/>"
+                + Math.round(series.percent) + "%</div>";
+      }
+	  
+    </script>	
+  </body>
+</html>
