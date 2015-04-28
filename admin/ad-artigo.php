6a3cc15a2337edba9acef38458ff894c7d2929b9
Diogo Novais <dnovais17@gmail.com>
2015-04-28 12:25:16 +0100

    Adicionar artigo

---
+++ admin/ad-artigo.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="UTF-8">
+	<title>Admin | Adicionar artigos</title>
+	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+	<!-- Bootstrap 3.3.2 -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<!-- Font Awesome Icons -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<!-- Ionicons -->
+	<link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+	<!-- Theme style -->
+	<link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+	<!-- AdminLTE Skins. Choose a skin from the css/skins 
+		 folder instead of downloading all of them to reduce the load. -->
+	<link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+  </head>
+  <body class="skin-blue">
+	<div class="wrapper">
+	  
+	<?php
+		include("cabecalho-admin.php");
+		include("lateral-admin.php");
+	?>
+
+	  <!-- Content Wrapper. Contains page content -->
+	  <div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+		  <h1>
+			Adicionar artigos
+			<small>descrição</small>
+		  </h1>
+		  <ol class="breadcrumb">
+			<li><a href="index.php"><i class="fa fa-dashboard"></i> Inicio</a></li>
+			<li><a href="gerir-artigos.php">Gestão de artigos</a></li>
+			<li class="active">Adicionar artigos</li>
+		  </ol>
+		</section>
+
+		<!-- Main content -->
+		<section class="content">
+		  <div class="row">
+			<div class="col-xs-12">
+			  <!-- general form elements -->
+			  <div class="box box-primary">
+				<div class="box-header">
+				  <h3 class="box-title">Dados do artigo</h3>
+				</div><!-- /.box-header -->
+				<!-- form start -->
+				<form role="form">
+				  <div class="box-body">
+					<div class="form-group">
+					  <label>Nome</label>
+					  <input type="text" class="form-control" placeholder="Insira o nome"/>
+					</div>
+					<div class="form-group">
+					  <label>Numero de inventário</label>
+					  <input type="text" class="form-control" placeholder="Insira o numero de inventário"/>
+					</div>
+					<div class="form-group">
+					  <label>Dimensões</label>
+					  <input type="text" class="form-control" placeholder="Insira as dimensões"/>
+					</div>
+					<div class="form-group">
+						<label>Datação</label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-calendar"></i>
+							</div>
+							<input class="form-control pull-right" id="reservation" type="text">
+						</div>
+					</div>
+					<div class="form-group">
+					  <label>Origem</label>
+					  <input type="text" class="form-control" placeholder="Insira a origem"/>
+					</div>
+					<div class="form-group">
+					  <label>Museu</label>
+					  <input type="text" class="form-control" placeholder="Insira o museu"/>
+					</div>
+					<div class="form-group">
+                      <label>Descrição</label>
+                      <textarea class="form-control" rows="3" placeholder="Insira uma descrição"></textarea>
+                    </div>
+					<div class="form-group">
+					  <label for="exampleInputFile">Fotografia</label>
+					  <input type="file" id="exampleInputFile">
+					  <p class="help-block">Seleccione uma fotografia para o artigo.</p>
+					</div>
+				  </div><!-- /.box-body -->
+
+				  <div class="box-footer">
+					<button type="submit" class="btn btn-primary">Guardar</button>
+				  </div>
+				</form>
+			  </div><!-- /.box -->
+			</div><!-- /.col -->
+		  </div>   <!-- /.row -->
+		</section><!-- /.content -->
+	  </div><!-- /.content-wrapper -->
+	  
+	  <?php
+		include("rodape-admin.php");
+	  ?>
+	</div><!-- ./wrapper -->
+
+	<!-- jQuery 2.1.3 -->
+	<script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+	<!-- Bootstrap 3.3.2 JS -->
+	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<!-- FastClick -->
+	<script src='plugins/fastclick/fastclick.min.js'></script>
+	<!-- AdminLTE App -->
+	<script src="dist/js/app.min.js" type="text/javascript"></script>
+	<!-- AdminLTE for demo purposes -->
+	<script src="dist/js/demo.js" type="text/javascript"></script>
+  </body>
+</html>
