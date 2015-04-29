51e90e0a4d1b2aacbff84dfd24392c2f47e0ec15
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-29 12:09:04 +0100

    adicionei ficheiros php para base de dados

---
+++ admin/conf.php
@@ -0,0 +1,9 @@
+<?php
+	// conf.php - Configurações da base de dados e do site
+
+	$conf_servidor = "localhost";
+	$conf_bd = "gm";
+	$conf_user = "root";
+	$conf_pass = "root";
+	$conf_lingua = "pt-pt";
+?>
