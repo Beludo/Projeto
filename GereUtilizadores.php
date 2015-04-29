2343f4c8a199f1c91b4ce02062d5a99db8256837
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-04-29 12:15:55 +0100

    atualização dos metodos a utilizar mas não está adaptado!!

--- GereUtilizadores.php
+++ GereUtilizadores.php
@@ -1,11 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Beludo
- * Date: 28-04-2015
- * Time: 10:26
- */
+
 include_once "GerePermissoes.php";
+include_once "acessobd.php";
+
 class GereUtilizadores {
 
     private $bd;
@@ -39,8 +36,88 @@ class GereUtilizadores {
 
         $gere->atribuiPermissao($registo['U_id'], $utilizador->getPermissao());
     }
+	
+	/*  POR FAZERR !!!!!!!! */
+			function alterarPalavraPasse($palavraPasse, $id) {
+				try {
+						$instrucao = $LigacaoBD->prepare("UPDATE Utilizadores SET (U_palavraPasse = ? WHERE U_id = ?) VALUES (?, ?)");
+						$instrucao->bind_param($palavraPasse, $id);
+						$sucesso_funcao = $instrucao->execute();
+						$instrucao->close();
+				} catch (PDOException $e) {
+						echo $e->getMessage();
+				}
+				if ($sucesso_funcao) {
+						return "True";
+				} else {
+						return "False";
+				}
+    	}
+	
+	/*  POR FAZERR !!!!!!!! */
 
-    function carregaUtilizador($id){
-
+    function verDadosUtilizador($id) {
+        try {
+            $registo = $this->bd->query("SELECT * FROM Utilizadores WHERE U_ID = :U_ID", $id);
+ 
+            if (isset($dados)) {
+                $utilizador = new Utilizadores($registo["u_id"], $registo["u_nome"], $registo["u_numerofuncionario"], 
+                                           $registo["u_nomeutilizador"], $registo["u_palavrapasse"], 
+                                           $registo["u_tipoutilizador"], $registo["u_dataregisto"], 
+                                           $registo["u_morada"], $registo["u_contatotelefonico"], 
+                                           $registo["u_datanascimento"], $registo["u_funcao"],
+                                           $registo["u_ativo"], $registo["u_fotografia"]);
+                return $utilizador;
+            }else{
+                return NULL;
+            }
+            
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
+	
+	/*  POR FAZERR !!!!!!!! */
+
+    function obtemUtilizadorUsername($nomeUtilizador) {
+        
+        try {
+            $user = array('U_NOMEUTILIZADOR' => $nomeUtilizador);
+            
+            $registo = $this->bd->query("SELECT * FROM Utilizadores WHERE U_NOMEUTILIZADOR = :U_NOMEUTILIZADOR", $user);
+ 
+            if (!$registo == null) {
+                $utilizador = new Utilizadores($registo[0]["U_ID"], $registo[0]["U_NOME"], $registo[0]["U_NUMEROFUNCIONARIO"],
+                                           $registo[0]["U_NOMEUTILIZADOR"], $registo[0]["U_PALAVRAPASSE"],
+                                           $registo[0]["U_TIPOUTILIZADOR"], $registo[0]["U_DATAREGISTO"],
+                                           $registo[0]["U_MORADA"], $registo[0]["U_CONTACTOTELEFONICO"],
+                                           $registo[0]["U_DATANASCIMENTO"], $registo[0]["U_FUNCAO"],
+                                           $registo[0]["U_ACTIVO"], $registo[0]["U_FOTOGRAFIA"]);
+                return $utilizador;
+            }else{
+                return NULL;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+	
+	/*  POR FAZERR !!!!!!!! */
+	
+    public function listarUtilizador(){
+		$dados = array();
+
+            $instrucao = $this->bd->query("SELECT * FROM utilizadores");
+
+        
+            
+            for($i=0; $i<count($instrucao); $i++){
+                	$dados[] = new Utilizadores($instrucao[$i]["U_ID"],$instrucao[$i]["U_NOME"],$instrucao[$i]["U_NUMEROFUNCIONARIO"],$instrucao[$i]["U_NOMEUTILIZADOR"],$instrucao[$i]["U_PALAVRAPASSE"],$instrucao[$i]["U_TIPOUTILIZADOR"],$instrucao[$i]["U_DATAREGISTO"],$instrucao[$i]["U_MORADA"],$instrucao[$i]["U_CONTACTOTELEFONICO"],$instrucao[$i]["U_DATANASCIMENTO"],$instrucao[$i]["U_FUNCAO"],$instrucao[$i]["U_ACTIVO"],$instrucao[$i]["U_FOTOGRAFIA"]);
+
+            }
+            return $dados;
+    }
+
+}
+
 } 
