e5d09f408129490807b6150051c7ff984760bb3d
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-05-12 15:30:42 +0100

    removi os funcionarios do menu lateral

--- admin/lateral-admin.php
+++ admin/lateral-admin.php
@@ -63,17 +63,6 @@
 			<li class="treeview">
 			  <a href="#">
 				<i class="fa fa-users"></i>
-				<span>Funcionarios</span>
-				<i class="fa fa-angle-left pull-right"></i>
-			  </a>
-			  <ul class="treeview-menu">
-				<li><a href="ad-funcionario.php"><i class="fa fa-user"></i> Adicionar funcionário</a></li>
-				<li><a href="gerir-funcionarios.php"><i class="fa fa-users"></i> Gerir funcionários</a></li>
-			  </ul>
-			</li>
-			<li class="treeview">
-			  <a href="#">
-				<i class="fa fa-users"></i>
 				<span>Utilizadores</span>
 				<i class="fa fa-angle-left pull-right"></i>
 			  </a>
