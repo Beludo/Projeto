76fa35cb21a41a8f36d2b07825e9bbaeb1279349
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-05-26 11:54:10 +0100

    esquecimento de partes! xD

--- login.php
+++ login.php
@@ -27,7 +27,7 @@
 
 
 			   <div class="login-box-body">
-        <p class="login-box-msg">Inicie sessão para entrar na área de administração</p>
+        <p class="login-box-msg">Inicie sessão para entrar na área de visitante</p>
         <form method="post" action="iniciaSessao.php">
           <div class="form-group has-feedback">
             <input type="text" class="form-control" placeholder="Username" name="username"/>
