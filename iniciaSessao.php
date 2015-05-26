0b4ba451801f70f86302a92cacd2f757efeeec99
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-05-26 11:15:23 +0100

    aqui é onde trata a sessão

---
+++ iniciaSessao.php
@@ -0,0 +1,64 @@
+<?php
+
+include_once "Visitantes.php";
+include_once "GereVisitante.php";
+
+session_start();
+// accao "logout" - terminar a sessão, se for pedido
+if(
+    isset($_GET["logout"]) && !empty($_GET["logout"]) &&
+    !strcmp($_GET["logout"], "1")
+){
+    $_SESSION = array();
+    if(isset($_COOKIE[session_name()])){
+        setcookie(session_name(), '', time()-42000, '/');
+    }
+    session_destroy();
+
+    header("Location: ./login.php?logout=1");
+    exit;
+}
+
+if(
+    isset($_POST["username"]) && !empty($_POST["username"]) &&
+    isset($_POST["password"]) && !empty($_POST["password"])){
+
+    // abrir ligação à base de dados
+    $bd = new BaseDados();
+    $gereVisitante = new GereVisitante();
+    $visitante = new Visitante(0, "", "", "", "", 0, "", "", "", 1, 1);
+
+    // carrega o utilizador com o username dado
+
+    $visitante = $gereVisitante->obtemVisitanteUsername($_POST["username"]);
+
+    // verifica se foi carregado um objeto na variável "utilizador"
+    if ($visitante == null){
+        header("Location: login.php?erro=1");
+    }else{
+
+        //verifica se o username que veio da base de dados é igual ao inserido
+        if(
+            !strcmp($_POST["username"], $utilizador->getUsername()) &&
+            !strcmp($_POST["password"], $utilizador->getPassword()) &&
+            $visitante->getAtivo() == 1){
+
+            // Guardar o nome de utilizador da sessão
+            $_SESSION["user"] = $visitante->getUsername();
+            echo $visitante->getPermissao();
+            // Verificar se se trata de um utilizador comum ou administrador
+            if($visitante->getPermissao() == 1){
+                header("Location: gerir-visitante.php");
+            } else {
+                header("Location: index.php");
+            }
+
+        }else{
+            header("Location: login.php?erro=1");
+        }
+    }
+}else{
+    header("Location: login.php?erro=2");
+}
+
+?>
