8f1ebec425c3ba8492bfc630670a9c6e697e9d02
Diogo Novais <dnovais17@gmail.com>
2015-06-04 02:22:12 +0100

    Código SQL para criar a tabela clima (corrigido)

---
+++ updates_base-dados/clima_04-06-2015_02.22.sql
@@ -0,0 +1,38 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.15-log - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.1.0.4867
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table gm.clima
+DROP TABLE IF EXISTS `clima`;
+CREATE TABLE IF NOT EXISTS `clima` (
+  `CL_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CL_ENDERECO_SENSOR` varchar(16) COLLATE utf8_bin NOT NULL,
+  `CL_DATAHORA` datetime NOT NULL,
+  `CL_TEMPERATURA` varchar(3) COLLATE utf8_bin NOT NULL,
+  `CL_HUMIDADE` varchar(4) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`CL_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Dumping data for table gm.clima: 5 rows
+/*!40000 ALTER TABLE `clima` DISABLE KEYS */;
+REPLACE INTO `clima` (`CL_ID`, `CL_ENDERECO_SENSOR`, `CL_DATAHORA`, `CL_TEMPERATURA`, `CL_HUMIDADE`) VALUES
+	(1, '2894E2DF020000FE', '2015-06-02 16:20:00', '25', '35'),
+	(2, '2894E2DF020000FE', '2015-06-10 00:00:00', '26', '50'),
+	(3, '002542E700E9E511', '2015-06-01 00:19:05', '11', '22'),
+	(4, '2894E2DF020000FE', '2015-06-20 16:40:00', '27', '30'),
+	(5, '2894E2DF020000FE', '2015-06-29 16:40:00', '35', '70'),
+	(6, '002542E700E9E511', '2015-06-03 00:19:05', '25', '40'),
+	(7, 'C0C0C0C00E9E511', '2015-06-07 00:19:05', '25', '40'),
+	(8, 'C0C0C0C00E9E511', '2015-06-05 00:19:05', '23', '28');
+/*!40000 ALTER TABLE `clima` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
