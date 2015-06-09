25a23287be55b237eac9a4a5fbef4d1c72b99ab9
DiogoAlexandre <lei133008@estgoh.ipc.pt>
2015-06-09 09:19:37 +0100

    Mudança de como muda as paginas!

--- loja.php
+++ loja.php
@@ -123,29 +123,11 @@ if(isset($_GET["ativo"]) && !empty($_GET["ativo"]) &&
 
 			<!-- Pagination -->
 			<div class="row text-center">
-				<div class="col-lg-12">
+				<div class="dataTables_paginate paging_bootstrap">
 					<ul class="pagination">
-						<li>
-							<a href="#">&laquo;</a>
-						</li>
-						<li class="active">
-							<a href="#">1</a>
-						</li>
-						<li>
-							<a href="#">2</a>
-						</li>
-						<li>
-							<a href="#">3</a>
-						</li>
-						<li>
-							<a href="#">4</a>
-						</li>
-						<li>
-							<a href="#">5</a>
-						</li>
-						<li>
-							<a href="#">&raquo;</a>
-						</li>
+							<li class="prev disabled"><a href="#">← Previous</a></li>
+							<li class="active"><a href="#">1</a></li>
+							<li class="next disabled"><a href="#">Next → </a></li>
 					</ul>
 				</div>
 			</div>
