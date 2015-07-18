813c963a62d59ba2dfaa03ca44beadf46c9f07e1
Beludo <marco_beludo_09@hotmail.com>
2015-07-18 17:22:01 +0100

    Permissões concluidas

--- admin/lateral-admin.php
+++ admin/lateral-admin.php
@@ -1,3 +1,9 @@
+<?php
+
+include_once "sessaoAtiva.php";
+
+?>
+
 <!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
 	<!-- sidebar: style can be found in sidebar.less -->
@@ -5,90 +11,135 @@
 		<!-- sidebar menu: : style can be found in sidebar.less -->
 		<ul class="sidebar-menu">
 			<li class="header">PAINEL DE CONTROLO</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-dashboard"></i>
-					<span>Geral</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="conf-gerais.php"><i class="fa fa-cog"></i> Configurações gerais</a>
-					</li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-files-o"></i>
-					<span>Artigos</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-artigo.php"><i class="fa fa-edit"></i> Adicionar Artigo</a>
-					</li>
-					<li><a href="gerir-artigos.php"><i class="fa fa-book"></i> Gerir Artigos</a>
-					</li>
-				</ul>
-			</li>
-			<li>
-				<a href="#">
-					<i class="fa fa-shopping-cart"></i>
-					<span>Loja</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-produto.php"><i class="fa fa-edit"></i> Adicionar Produto</a>
-					</li>
-					<li><a href="gerir-produtos.php"><i class="fa fa-book"></i> Gerir Produtos</a>
-					</li>
-                    <li><a href="encomendas.php"><i class="fa fa-book"></i> Gerir Encomendas</a>
+
+            <?php
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1){
+                ?>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-dashboard"></i>
+                            <span>Geral</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="conf-gerais.php"><i class="fa fa-cog"></i> Configurações gerais</a>
+                            </li>
+                        </ul>
                     </li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-bar-chart-o"></i>
-					<span>Registo climatico</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="graficos-hora.php"><i class="fa fa-clock-o"></i>Por hora</a>
-					</li>
-					<li><a href="graficos-intervalo.php"><i class="fa fa-calendar"></i>Intervalo de Tempo</a>
-					</li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-users"></i>
-					<span>Utilizadores</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-utilizador.php"><i class="fa fa-user"></i> Adicionar utilizador</a>
-					</li>
-					<li><a href="gerir-utilizadores.php"><i class="fa fa-users"></i> Gerir utilizadores</a>
-					</li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-users"></i>
-					<span>Visitantes</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="gerir-visitantes.php"><i class="fa fa-users"></i> Gerir Visitantes</a>
-					</li>
-					<li><a href="gerir-quotas.php"><i class="fa fa-eur"></i> Quotas de Sócios</a>
-					</li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="log-utilizadores.php">
-					<i class="fa fa-archive"></i>
-					<span>Log</span>
-				</a>
-			</li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1 || $_SESSION["permissoes"][$i] == 4){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-files-o"></i>
+                        <span>Artigos</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-artigo.php"><i class="fa fa-edit"></i> Adicionar Artigo</a>
+                        </li>
+                        <li><a href="gerir-artigos.php"><i class="fa fa-book"></i> Gerir Artigos</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1 || $_SESSION["permissoes"][$i] == 2){
+                ?>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-shopping-cart"></i>
+                        <span>Loja</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-produto.php"><i class="fa fa-edit"></i> Adicionar Produto</a>
+                        </li>
+                        <li><a href="gerir-produtos.php"><i class="fa fa-book"></i> Gerir Produtos</a>
+                        </li>
+                        <li><a href="encomendas.php"><i class="fa fa-book"></i> Gerir Encomendas</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1 || $_SESSION["permissoes"][$i] == 3){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-bar-chart-o"></i>
+                        <span>Registo climatico</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="graficos-hora.php"><i class="fa fa-clock-o"></i>Por hora</a>
+                        </li>
+                        <li><a href="graficos-intervalo.php"><i class="fa fa-calendar"></i>Intervalo de Tempo</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-users"></i>
+                        <span>Utilizadores</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-utilizador.php"><i class="fa fa-user"></i> Adicionar utilizador</a>
+                        </li>
+                        <li><a href="gerir-utilizadores.php"><i class="fa fa-users"></i> Gerir utilizadores</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1 || $_SESSION["permissoes"][$i] == 6){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-users"></i>
+                        <span>Visitantes</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="gerir-visitantes.php"><i class="fa fa-users"></i> Gerir Visitantes</a>
+                        </li>
+                        <li><a href="gerir-quotas.php"><i class="fa fa-eur"></i> Quotas de Sócios</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1){
+                ?>
+                <li class="treeview">
+                    <a href="log-utilizadores.php">
+                        <i class="fa fa-archive"></i>
+                        <span>Log</span>
+                    </a>
+                </li>
+                <?php
+                }
+            }
+            ?>
 			<li class="treeview">
 				<a href="mailbox.html">
 					<i class="fa fa-envelope"></i> <span>Mensagens</span>
@@ -116,66 +167,92 @@
 			  </ul>
 			</li>
 			-->
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-camera"></i> <span>Visita virtual</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-fotos.php"><i class="fa fa-circle-o"></i> Adicionar fotos</a>
-					</li>
-					<li><a href="gerir-visita.php"><i class="fa fa-circle-o"></i> Gerir visita virtual</a>
-					</li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-bars"></i> <span>Exposições</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-exposicoes.php"><i class="fa fa-circle-o"></i> Adicionar Exposições</a>
-					</li>
-					<li><a href="gerir-exposicoes.php"><i class="fa fa-circle-o"></i> Gerir Exposições</a>
-					</li>
-					<li class="treeview">
-				<a href="#">
-					<i class="fa fa-bars"></i> <span>Tipo de Exposições</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-tipoexp.php"><i class="fa fa-circle-o"></i> Adicionar Tipo de Exposições</a>
-					</li>
-					<li><a href="gerir-tipoexp.php"><i class="fa fa-circle-o"></i> Gerir Tipo de Exposições</a>
-					</li>
-				</ul>
-			</li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa  fa-calendar-o"></i> <span>Eventos</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-eventos.php"><i class="fa fa-edit"></i> Adicionar Eventos</a>
-					</li>
-					<li><a href="gerir-eventos.php"><i class="fa fa-book"></i> Gerir Eventos</a>
-					</li>
-				</ul>
-			</li>
-			<li class="treeview">
-				<a href="#">
-					<i class="fa  fa-calendar-o"></i> <span>Parcerias</span>
-					<i class="fa fa-angle-left pull-right"></i>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="ad-parceria.php"><i class="fa fa-edit"></i> Adicionar Parcerias</a>
-					</li>
-					<li><a href="gerir-parcerias.php"><i class="fa fa-book"></i> Gerir Parcerias</a>
-					</li>
-				</ul>
-			</li>
+            <?php
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1 || $_SESSION["permissoes"][$i] == 7){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-camera"></i> <span>Visita virtual</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-fotos.php"><i class="fa fa-circle-o"></i> Adicionar fotos</a>
+                        </li>
+                        <li><a href="gerir-visita.php"><i class="fa fa-circle-o"></i> Gerir visita virtual</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1 || $_SESSION["permissoes"][$i] == 5){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-bars"></i> <span>Exposições</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-exposicoes.php"><i class="fa fa-circle-o"></i> Adicionar Exposições</a>
+                        </li>
+                        <li><a href="gerir-exposicoes.php"><i class="fa fa-circle-o"></i> Gerir Exposições</a>
+                        </li>
+                        <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-bars"></i> <span>Tipo de Exposições</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-tipoexp.php"><i class="fa fa-circle-o"></i> Adicionar Tipo de Exposições</a>
+                        </li>
+                        <li><a href="gerir-tipoexp.php"><i class="fa fa-circle-o"></i> Gerir Tipo de Exposições</a>
+                        </li>
+                    </ul>
+                </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1 || $_SESSION["permissoes"][$i] == 3){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa  fa-calendar-o"></i> <span>Eventos</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-eventos.php"><i class="fa fa-edit"></i> Adicionar Eventos</a>
+                        </li>
+                        <li><a href="gerir-eventos.php"><i class="fa fa-book"></i> Gerir Eventos</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            for($i=0; $i < sizeof($_SESSION["permissoes"]); $i++){
+                if($_SESSION["permissoes"][$i] == 1){
+                ?>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa  fa-calendar-o"></i> <span>Parcerias</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="ad-parceria.php"><i class="fa fa-edit"></i> Adicionar Parcerias</a>
+                        </li>
+                        <li><a href="gerir-parcerias.php"><i class="fa fa-book"></i> Gerir Parcerias</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                }
+            }
+            ?>
 			<li class="treeview">
 				<a href="#">
 					<i class="fa fa-files-o"></i>
--- admin/login.php
+++ admin/login.php
@@ -2,6 +2,8 @@
 
 include_once "Utilizadores.php";
 include_once "GereUtilizadores.php";
+include_once "Permissoes.php";
+include_once "GerePermissoes.php";
 
 session_start();
 // accao "logout" - terminar a sessão, se for pedido
@@ -27,6 +29,7 @@ if(
     $bd = new BaseDados();
     $gereUtilizador = new GereUtilizadores();
     $utilizador = new Utilizadores(0, "", "", "", "", 0, "", "", "", 1, 1);
+    $gerePermissoes = new GerePermissoes();
 
     // carrega o utilizador com o username dado
 
@@ -48,13 +51,28 @@ if(
 			
 			// Guardar o id de utilizador da sessão
 			$_SESSION["iduser"] = $utilizador->getId();
-			
+
+            // Guardar as permissoes do utilizador
+            $_SESSION["permissoes"] = $gerePermissoes->permissoesUtilizador($utilizador->getId());
+
             echo $utilizador->getPermissao();
             // Verificar se se trata de um utilizador comum ou administrador
             if($utilizador->getPermissao() == 1){
                 header("Location: gerir-utilizadores.php");
+            } elseif($utilizador->getPermissao() == 2) {
+                header("Location: gerir-produtos.php");
+            } elseif($utilizador->getPermissao() == 3){
+                header("Location: gerir-eventos.php");
+            } elseif($utilizador->getPermissao() == 4){
+                header("Location: gerir-produtos.php");
+            } elseif($utilizador->getPermissao() == 5){
+                header("Location: gerir-artigos.php");
+            } elseif($utilizador->getPermissao() == 6){
+                header("Location: gerir-quotas.php");
+            } elseif($utilizador->getPermissao() == 7){
+                header("Location: gerir-visita.php");
             } else {
-                header("Location: loja.php");
+                header("Location: index.php?erro=1");
             }
 
         }else{
